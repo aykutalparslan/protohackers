60a5a19b6febcccc50bf9f8bf9661d6788568c27
Aykut Alparslan KOC <aykutalparslan@msn.com>
2022-12-07 19:53:04 +0300

    do not complete input

--- PrimeTime.cs
+++ PrimeTime.cs
@@ -37,7 +37,7 @@ public class PrimeTime : TcpServerBase
                 break;
             }
         }
-        connection.Input.Complete();
+        
         await connection.Output.CompleteAsync();
     }
 
