77e280dce6c1fc403d7e84230d85c7ddec6a10a4
Aykut Alparslan KOC <aykutalparslan@msn.com>
2022-12-07 20:12:45 +0300

    smoke test

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 using System.Security.Cryptography;
 using protohackers;
 
-var smokeTest = new PrimeTime();
+var smokeTest = new SmokeTest();
 await smokeTest.Start(12345);
 
 //var primeTime = new PrimeTime();
